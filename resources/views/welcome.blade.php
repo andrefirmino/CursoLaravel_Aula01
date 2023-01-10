174266349237ec6ad43ac36443e6108fc0f73e81
andrecnl <andrecnl@gitlab.com>
2023-01-09 22:06:01 -0300

    exercicio 02

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Documento</title>
+    <title>Templates</title>
 </head>
 <body>
-    <h1>Olá Mundo!</h1>
-    <h2>Seja bem-vindo</h2>
+    <h1>Meu primeiro template Blade</h1>
 </body>
 </html>
